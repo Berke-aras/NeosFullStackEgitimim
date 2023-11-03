c8bb3f97b852363e186d37b27c0088272e5835c9
Berke <berkearastaman@outlook.com>
2023-11-03 19:55:04 +0300

    js1

---
+++ Neos Web Eğitim/HAFTA4/Ders4/js-giris/main.js
@@ -0,0 +1,33 @@
+console.log("Hello World");
+
+// yorum satır
+
+/*
+Çoklu Yorum Satırı
+*/
+
+
+//alert - İçe Gömülü Bir Fonksiyondur. Kullanıcıya mesaj göndermek için kullanılır
+// Console llog içe gömülü bir fonksiyondur. Console kısmına çıktı göndermek için kullanılır
+
+// eski değişken oluşturma yöntemi
+// var degislenAdi = "Merhaba Dünya";
+
+// console.log(degislenAdi);
+// console.log("Merhaba Dünya");
+
+//JavaScipt ES6
+
+let degisken1 = "Merhaba Dünya";
+console.log(degisken1);
+
+degisken1 = "Güle Güle Dünya"
+console.log(degisken1);
+
+//const degisken degisemez
+const degisken2 = "Selam";
+console.log(degisken2);
+
+degisken2 = "Görüşürüz";
+console.log(degisken2);
+
