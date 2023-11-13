ef4433a3e2de5b47d76853a531c6a8eaf5b00274
Berke <berkearastaman@outlook.com>
2023-11-13 19:53:06 +0300

    gnc

--- Neos Web Eğitim/HAFTA5/ders1/main.js
+++ Neos Web Eğitim/HAFTA5/ders1/main.js
@@ -18,6 +18,26 @@ const para = document.querySelector("p");
 const para2 = document.querySelector(".description");
 const para3 = document.querySelector("#main-description");
 
-const para4 = document.querySelectorAll(".description"); //descripton etiketine sahip tüm elementleri alıyor
+const para4 = document.querySelectorAll(".description"); //descripton etiketine sahip tüm elementleri alıyor.
 
 console.log(para + "\n" + para2 + "\n" + para3);
+
+para3.innerHTML = "Merhaba";
+para3.innerHTML += "Merhaba"; // ekleme yapar +=
+
+const input = document.getElementById("input-alani");
+const inpu2 = document.querySelector("#input-alani"); // iki seçim yöntemide olur.
+input.placeholder = "Merhabaa";
+input.id = "input";
+
+let saat = new Date();
+console.log(saat);
+
+document.getElementById("saat-gosterim").innerHTML = saat; // tek sefer kullanma da bune benzer bir kullanım yapılabilir boşa yer kaplamaz
+//veya
+document.getElementById("saat-gosterim").innerHTML = new Date();
+//
+
+const container = document.querySelector("#container");
+
+container.innerHTML = "<h1 class='baslik'>Bu bir Başlık</h1>";
