f2337101748ea18d10c30a8143c6c78a64f86b35
Berke <berkearastaman@outlook.com>
2023-12-04 23:47:28 +0300

    py odev

--- Neos Web Eğitim/HAFTA8/Ders1/main.py
+++ Neos Web Eğitim/HAFTA8/Ders1/main.py
@@ -1,9 +1,6 @@
 # armstrong sayı
 # 153 = 1**3 + 5**3 + 3**3 - eğer eşitse armstron sayi
 # 1416 = 1**4 + 4**4 + 1**4 + 6**4
-
-
-
 def armstrong(sayi):
     sayiUzunluk = len(str(sayi))
 
@@ -22,17 +19,34 @@ def armstrong(sayi):
 # sayi = input("Sayi Girin: ")
 # armstrong(sayi)
 
+
+
 #girilen sayı asal mı 1 ile kendisi hariç hiç bir sayıya bölünemez
 
-sayi = 15
+def asalmi(sayi):
+    sayi = int(sayi)
+    asal = True
+    for i in range(2, sayi):
+        if sayi == 0 or sayi == 1:
+            print("Asal Değil")
+            asal = False
+            break
+        if sayi == 2:
+            asal = True
+            break
+        elif sayi % i == 0:
+            print("Asal Değil")
+            asal = False
+            break
+    if asal == True:
+        print("Asal")
 
-for i in range(2, sayi):
-    if sayi :
-        pass
+# sayi = input("Sayı: ")
+# asalmi(sayi)
+
 
 
 #99 dakuza kadar kelimlerle yazdırma
-
 def sayiYazdir(sayi):
     birler = ["sıfır", "bir", "iki", "üç", "dört","beş", "altı", "yedi", "sekiz", "dokuz"]
     onlar = ["on", "yirmi","otuz","kırk","elli","altmış","yetmiş","seksen","doksan"]
@@ -57,5 +71,5 @@ def sayiYazdir(sayi):
     else:
         print("Hatalı Giriş")
 
-sayi = input("Sayi Girin: ")
-sayiYazdir(sayi)
+# sayi = input("Sayi Girin: ")
+# sayiYazdir(sayi)
