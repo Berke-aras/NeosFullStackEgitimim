1c10078e7dc2f10a60924fea3dec9461e36f655f
Berke <berkearastaman@outlook.com>
2023-12-26 21:24:53 +0300

    react_updt

--- Neos Web Eğitim/HAFTA11/react2/src/App.jsx
+++ Neos Web Eğitim/HAFTA11/react2/src/App.jsx
@@ -2,25 +2,58 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Input from "./input";
 import PersonInfo from "./PersonInfo";
 import HighComponent from "./HighComponent";
+import NameShow from "./NameShow";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
     const name = "Berke";
     const surname = "Arastaman";
+
+    // const isimler = ["Ercüment", "Berke", "Arastaman"];
+    const [isimler, setIsimler] = useState(["Ercüment", "Berke", "Arastaman"]);
+
+    function handleClick() {
+        // isimler.push("Barış");
+        // console.log(isimler);
+
+        const copyNames = [...isimler]; // ... = kopyalama - atadığımızda ana değerleri silmemesi için
+        copyNames.push("barış");
+        setIsimler(copyNames);
+    }
+
     return (
         <>
-            <div className="w-50 h-50">
+            {isimler.map((isim, i) => {
+                return <NameShow isim={isim} key={i} />;
+            })}
+
+            <button onClick={handleClick}>buton</button>
+
+            <br />
+            <hr />
+            <br />
+
+            <div>
                 <button className="btn btn-danger">Tıkla</button>
             </div>
 
+            <br />
+            <hr />
+            <br />
+
             <HighComponent />
+
             <br />
             <hr />
             <br />
+
             <PersonInfo name={name} soyad={surname} age={22} />
+
             <br />
             <hr />
             <br />
+
             <Input />
         </>
     );
---
+++ Neos Web Eğitim/HAFTA11/react2/src/NameShow.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NameShow(props) {
+    return (
+        <>
+            <div>{props.isim}</div>
+        </>
+    );
+}
+
+export default NameShow;
