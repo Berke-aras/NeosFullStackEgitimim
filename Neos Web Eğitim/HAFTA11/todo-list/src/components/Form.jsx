1d95de4ca02d0cd24987253115443f4455eab4fc
Berke <berkearastaman@outlook.com>
2023-12-31 14:35:11 +0300

    updt

--- Neos Web Eğitim/HAFTA11/todo-list/src/components/Form.jsx
+++ Neos Web Eğitim/HAFTA11/todo-list/src/components/Form.jsx
@@ -1,7 +1,30 @@
 import { FaCirclePlus } from "react-icons/fa6";
 import React from "react";
 
-function Form() {
+function Form({ inputText, setInputText, todos, setTodos, setStatus }) {
+    const submitToDoHandler = (e) => {
+        e.preventDefault();
+        setTodos([
+            ...todos,
+            { text: inputText, complated: false, id: Math.random() },
+        ]);
+        //üç nokta öperatörü elimizdeki verileri tutmamızı sağlıyor
+
+        console.log(todos);
+
+        setInputText("");
+    };
+
+    const inputTextHandler = (e) => {
+        // console.log(e.target.value);
+        setInputText(e.target.value);
+    };
+
+    const statusHelper = (e) => {
+        setStatus(e.target.value);
+        console.log(e.target.value);
+    };
+
     return (
         <>
             <form>
@@ -10,23 +33,27 @@ function Form() {
                         className="todo-input"
                         type="text"
                         placeholder="Add..."
+                        onChange={inputTextHandler}
+                        value={inputText}
                     />
-                    <button className="todo-button" type="submit">
+                    <button
+                        className="todo-button"
+                        type="submit"
+                        onClick={submitToDoHandler}
+                    >
                         <FaCirclePlus />
                     </button>
                 </div>
 
                 <div className="select">
-                    <select name="todos" className="filter-todo">
-                        <option value="" key="">
-                            All
-                        </option>
-                        <option value="" key="">
-                            Complated
-                        </option>
-                        <option value="" key="">
-                            UnComplated
-                        </option>
+                    <select
+                        name="todos"
+                        className="filter-todo"
+                        onChange={statusHelper}
+                    >
+                        <option value="all">All</option>
+                        <option value="complated">Complated</option>
+                        <option value="uncomplated">UnComplated</option>
                     </select>
                 </div>
             </form>
