b79506ff850868dae552adadee25320cc204ae16
Berke <berkearastaman@outlook.com>
2024-01-16 20:56:12 +0300

    updatee

--- Neos Web Eğitim/HAFTA15-TekrarveEkler/Django/ilkDjango/ilkDjango/urls.py
+++ Neos Web Eğitim/HAFTA15-TekrarveEkler/Django/ilkDjango/ilkDjango/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path
 
 from myApp.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name="index"),
     path('detay/<id>', detay, name="detay" )
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Neos Web Eğitim/HAFTA15-TekrarveEkler/Django/ilkDjango/myApp/models.py
+++ Neos Web Eğitim/HAFTA15-TekrarveEkler/Django/ilkDjango/myApp/models.py
@@ -6,6 +6,7 @@ class Urunler(models.Model):
     isim = models.CharField(max_length=100, verbose_name="Urun İsmi")
     aciklama=models.TextField(max_length=500)
     fiyat=models.IntegerField(default=0)
+    resim=models.FileField(upload_to="urunResimleri", null=True, blank=True) #klasör adı
     
     def __str__(self):
         return self.isim
