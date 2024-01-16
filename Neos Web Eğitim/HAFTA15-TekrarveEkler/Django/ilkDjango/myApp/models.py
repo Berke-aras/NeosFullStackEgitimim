84a294ec80d199d36971445483aaceeb6587407f
Berke <berkearastaman@outlook.com>
2024-01-16 20:42:29 +0300

    updt

--- Neos Web Eğitim/HAFTA15-TekrarveEkler/Django/ilkDjango/myApp/models.py
+++ Neos Web Eğitim/HAFTA15-TekrarveEkler/Django/ilkDjango/myApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Urunler(models.Model):
+    isim = models.CharField(max_length=100, verbose_name="Urun İsmi")
+    aciklama=models.TextField(max_length=500)
+    fiyat=models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.isim
+    
--- Neos Web Eğitim/HAFTA15-TekrarveEkler/Django/ilkDjango/myApp/views.py
+++ Neos Web Eğitim/HAFTA15-TekrarveEkler/Django/ilkDjango/myApp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+from .models import *
 # Create your views here.
+
+def index(request):
+    urunler = Urunler.objects.all()
+    context = {
+        "urunler": urunler
+    }
+    
+    return render(request, "index.html", context)
+
+def detay(request, id):
+    urun = Urunler.objects.filter(id=id)
+    context = {
+        "urun": urun
+    }
+    return render(request, "detay.html", context)
